eef8ca8b32ad64513515020482e90ce7c0eb9e2d
sanjayntms <sanjayntms@yahoo.com>
2020-01-28 17:00:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -34,7 +34,7 @@ if($_POST['submit']) {
 	<title>NTMS Training Institute-Azure Batch-PHP-SQL Login</title>
 </head>
 <body>
-<h1>Login</h1>
+<h1>NTMS Training Institute-Azure Batch Login</h1>
 <form method="post" action="index.php">
 	<input type="text" name = "username" placeholder="Enter username">
 	<input type="password" name="password" placeholder="Enter password here">
