42777d36a3ec48644d0bd1969a5e5ce5c4914182
sanjayntms <sanjayntms@yahoo.com>
2020-01-28 15:46:03 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php
-$db = mysqli_connect("ntmssql1.mysql.database.azure.com", "sqladmin", "ntMS123#", "login") or die ("Failed to connect");
+$db = mysqli_connect("ntmssql1.mysql.database.azure.com", "sqladmin@ntmssql1", "ntMS123#", "login") or die ("Failed to connect");
 
 ?>
